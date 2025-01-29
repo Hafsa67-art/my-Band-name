4890a9320309110a42a8dbb4a86b26f5a43fdccc
Hafsa67-art <83156267+Hafsa67-art@users.noreply.github.com>
2025-01-29 14:51:18 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,4 @@
+print("WELCOME TO THE BAND NAME GENERATOR \n")
+city=input("which city did you grow up in?\n")
+pet=input("what is the name of pet\n ")
+print ("your brand name could be :"+ city +" "+pet )
